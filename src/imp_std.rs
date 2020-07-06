d0fcd6dd50227307982df3aa9a7a1ca771cfc860
Liwei Guo <zaxguo@me.com>
2020-07-06 14:11:37 +0000

    port to mesalock_sgx

--- src/imp_std.rs
+++ src/imp_std.rs
@@ -3,14 +3,23 @@
 //   * no poisoning
 //   * init function can fail
 
+
+use std::prelude::v1::*;
 use std::{
     cell::{Cell, UnsafeCell},
     hint::unreachable_unchecked,
     marker::PhantomData,
     panic::{RefUnwindSafe, UnwindSafe},
     sync::atomic::{AtomicBool, AtomicUsize, Ordering},
-    thread::{self, Thread},
 };
+#[cfg(not(feature = "mesalock_sgx"))]
+use std::thread::{self, Thread};
+
+#[cfg(feature = "mesalock_sgx")]
+use std::thread::{self, SgxThread as Thread};
+
+
+
 
 #[derive(Debug)]
 pub(crate) struct OnceCell<T> {
--- src/lib.rs
+++ src/lib.rs
@@ -232,6 +232,13 @@ might be easier to debug than a deadlock.
 */
 
 #![cfg_attr(not(feature = "std"), no_std)]
+#![cfg_attr(all(feature = "mesalock_sgx",
+                not(target_env = "sgx")), no_std)]
+#![cfg_attr(all(target_env = "sgx", target_vendor = "mesalock"), feature(rustc_private))]
+
+#[cfg(all(feature = "mesalock_sgx", not(target_env = "sgx")))]
+#[macro_use]
+extern crate sgx_tstd as std;
 
 #[cfg(feature = "std")]
 #[cfg(feature = "parking_lot")]
